c77183a3927f899a1650291387363e2d25e04e6f
joao <vitordire@gmail.com>
2024-11-21 20:44:57 -0300

    remocao do footer

--- src/components/footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        <p>© 2024 Calculadora Solar. Todos os direitos reservados.</p>
-      </footer>
-    );
-  }
-  
